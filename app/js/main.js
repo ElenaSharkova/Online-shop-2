6de45a00d0f7287873fa3b2a75922430f2075b1f
sharkova.ev <sharkova.ev@phystech.edu>
2021-07-21 23:48:01 +0300

    Первый раздел главной страницы

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
-
+   $('.top__slider').slick({
+        dots: true, //врубили дотсы
+        arrows: false, // вырубили стрелочки
+        fade: true, // чтобы менялось плавно
+        autoplay: true
+        
+    });
+    
+    
 })
