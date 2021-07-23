84b5200911c1be4c3d101a82147cc9fac458083c
sharkova.ev <sharkova.ev@phystech.edu>
2021-07-23 12:14:54 +0300

    Добавлен слайдер основного контента

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,13 @@ $(function () {
         autoplay: true
         
     });
+    $('.content__slider').slick({
+        //dots: true, //врубили дотсы
+        arrows: false, // вырубили стрелочки
+        fade: true, // чтобы менялось плавно
+        autoplay: true
+        
+    });
     
     
 })
