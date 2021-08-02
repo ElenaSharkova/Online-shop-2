4182f340935d2198e5a1674441c78219bfeddf14
sharkova.ev <sharkova.ev@phystech.edu>
2021-08-02 16:40:06 +0300

    Обнавлено меню страницы товара

--- app/js/main.js
+++ app/js/main.js
@@ -38,5 +38,11 @@ $(function () {
         $($(this).attr('href')).addClass('menu-tabs__content--active');
     });
     
-    
+    $('.locals-menu__item').on('click', function (e){
+        e.preventDefault(); //ссылки отменяют себе переход на другую страницу
+        $('.locals-menu__item').removeClass('locals-menu__item--active');
+        $(this).addClass('locals-menu__item--active');
+        $('.locals-menu__content').removeClass('locals-menu__content--active'); //убрали вообще весь контент из любого таба
+        $($(this).attr('href')).addClass('locals-menu__content--active');
+    });
 })
